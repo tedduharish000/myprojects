99fe7c591904ede1dbd46cbc33424e65d6ae55db
teddu_harish <tedduharish000@gmail.com>
2024-11-02 13:14:49 +0530

    new

--- indiaproject/india.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-# Create a Spark session
-spark = SparkSession.builder.appName("PySparkTutorial").getOrCreate()
-
-# Print Spark session details
-print(spark.version)
